201c2b5d331cb6a8119b5d025ca15e59021676fb
Ankit Kumar <ankitraj1693@gmail.com>
2025-02-07 11:51:44 +0530

    nav updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Nav";
+import Navbar from "@/components/Navbar";
 import Swap from "@/components/Swap";
 
 export const metadata = {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,21 @@
 "use client";
-import React, { useState } from "react";
-import { ethers } from "ethers";
-import toast, { Toaster } from 'react-hot-toast';
+import React, { useContext } from "react";
+import { WalletContext } from "./ConnectWallet/WalletContext"
+import toast, { Toaster } from "react-hot-toast";
 import Link from "next/link";
 
-const Navbar = () => {
-    const [account, setAccount] = useState(null);
-
-    const connectWallet = async () => {
-        if (typeof window.ethereum !== "undefined") {
-            try {
-                const provider = new ethers.BrowserProvider(window.ethereum);
-                const accounts = await provider.send("eth_requestAccounts", []);
-                setAccount(accounts[0]);
-                toast.success("Wallet Connected!")
-            } catch (error) {
-                console.error("User denied account access", error);
-            }
-        } else {
-            toast.error("MetaMask is not installed. Please install MetaMask to continue.");
-        }
-    };
-
-    const disconnectWallet = () => {
-        setAccount(null);
-        toast.success('Wallet Disconnected')
-    };
+const Nav = () => {
+    const { account, connectWallet, disconnectWallet } = useContext(WalletContext);
 
     return (
+        
         <div className="bg-gray-800">
+            <Toaster />
             <div className="container py-3 px-2 md:px-20 text-xl flex justify-between text-white items-center">
-                <Link href={'/'}>
-                <h1 className="text-center text-white text-lg md:text-2xl font-medium" style={{ fontFamily: "poppins" }}>
-                    Hommies <span className="text-gray-400 font-bold">Swap</span>
-                </h1>
+                <Link href={"/"}>
+                    <h1 className="text-center text-white text-lg md:text-2xl font-medium" style={{ fontFamily: "poppins" }}>
+                        Hommies <span className="text-gray-400 font-bold">Swap</span>
+                    </h1>
                 </Link>
 
                 {account ? (
@@ -44,7 +26,7 @@ const Navbar = () => {
                         <button
                             onClick={disconnectWallet}
                             className="bg-red-500 px-4 py-2 text-base font-semibold rounded-lg text-white hover:bg-red-600"
-                            style={{fontFamily:"poppins"}}
+                            style={{ fontFamily: "poppins" }}
                         >
                             Logout
                         </button>
@@ -53,15 +35,14 @@ const Navbar = () => {
                     <button
                         onClick={connectWallet}
                         className="bg-gray-300 px-4 py-2 text-base font-semibold rounded-md text-black hover:text-white hover:bg-gray-500"
-                        style={{fontFamily:"poppins"}}
+                        style={{ fontFamily: "poppins" }}
                     >
                         Connect
                     </button>
                 )}
+            </div>
         </div>
-        </div>
-        
     );
 };
 
-export default Navbar;
+export default Nav;
